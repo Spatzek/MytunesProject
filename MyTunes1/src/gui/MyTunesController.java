07745817cdf7332b1a2adb0f216dc30d1ec0bad4
KasperHN <kasper_130195@live.dk>
2019-11-25 13:59:00 +0100

    yes

--- MyTunes1/src/gui/MyTunesController.java
+++ MyTunes1/src/gui/MyTunesController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mytunes1;
+package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
