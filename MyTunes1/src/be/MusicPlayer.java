8c86c0b8d18cffa22bc2f43023e2424a648c2427
KasperHN <kasper_130195@live.dk>
2019-11-26 13:43:34 +0100

    k

--- MyTunes1/src/be/MusicPlayer.java
+++ MyTunes1/src/be/MusicPlayer.java
@@ -179,12 +179,12 @@ public class MusicPlayer
             player.play();
 
             // after 5 secs, pause
-            Thread.sleep(5000);
-            player.pause();     
+            //Thread.sleep(5000);
+            //player.pause();     
 
             // after 5 secs, resume
-            Thread.sleep(5000);
-            player.resume();
+            //Thread.sleep(5000);
+            //player.resume();
         } 
         catch (final Exception e) 
         {
